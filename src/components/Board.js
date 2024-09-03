018f4a4cd11d0851b86eb71ef014222fdf54c2f6
Yudi Matsuguma <yudimatsuguma@gmail.com>
2024-09-02 21:55:48 -0300

    refactor: implementado useReducer

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,41 +1,60 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import TaskInput from "./TaskInput";
 import TaskTable from "./TaskTable";
 
 export const ActionsContext = createContext();
 
+function taskReducer(state, action) {
+  switch (action.type) {
+    case 'add': {
+      return [...state, { id: action.id, title: action.title, done: false }]
+    }
+    case 'updateStatus': {
+      return state.map(item => item.id === action.id ? { ...item, done: !item.done } : item)
+    }
+    case 'edit': {
+      return state.map(item => item.id === action.id ? { ...item, title: action.title } : item)
+    }
+    case 'delete': {
+      return state.filter(item => item.id !== action.id)
+    }
+    default: {
+      throw new Error('Invalid action type!')
+    }
+  }
+}
+
 export default function Board() {
-  const [data, setData] = useState([]);
+  const [data, dispatch] = useReducer(taskReducer, []);
 
   function createTask(title) {
-    const id = crypto.randomUUID();
-    const newData = data.concat({ id, title, done: false });
-
-    setData(newData);
+    dispatch({
+      type: 'add',
+      id: crypto.randomUUID(),
+      title,
+    })
   }
 
   function handleTaskStatusChange(id) {
-    setData(data.map(item => {
-      if (item.id === id) {
-        return { ...item, done: !item.done }
-      } else {
-        return item;
-      }
-    }))
+    dispatch({
+      type: 'updateStatus',
+      id
+    })
   }
 
   function handleTaskUpdate(id, title) {
-    setData(data.map(item => {
-      if (item.id === id) {
-        return { ...item, title }
-      } else {
-        return item;
-      }
-    }))
+    dispatch({
+      type: 'edit',
+      id,
+      title
+    })
   }
 
   function handleTaskDelete(id) {
-    setData(data.filter(item => item.id !== id))
+    dispatch({
+      type: 'delete',
+      id
+    })
   }
 
   return (
